06a4f26ece826e5096ca8ad20025696959986b13
DanishQuamarKhan <2141998@cognizant.com>
2024-04-17 23:47:47 +0530

    Test case added for Home

--- app/__test__/page.test.tsx
+++ app/__test__/page.test.tsx
@@ -13,3 +13,43 @@ test('Displays NullData component for empty category filter results', () => {
     const nullData = screen.getByText(/Oops! No Products found/i);
     expect(nullData).toBeInTheDocument(); // Assert NullData component is rendered
   });
+
+  test('Displays filtered products by category', () => {
+    const searchParams = { category: 'Phone' };
+    const filteredProducts = products.filter(product => product.category === 'Phone');
+  
+    render(<Home searchParams={searchParams} />);
+  
+    const productCards = screen.getAllByTestId('product-card');
+    expect(productCards.length).toBe(filteredProducts.length); // Assert number of product cards matches filtered products
+  });
+
+test('Displays NullData component for empty search term results', () => {
+    const searchParams = { searchTerm: 'unavailable' };
+  
+    render(<Home searchParams={searchParams} />);
+  
+    const nullData = screen.getByText(/Oops! No Products found/i);
+    expect(nullData).toBeInTheDocument(); // Assert NullData component is rendered
+  });
+
+  test('Displays filtered products by search term', () => {
+    const mockSearchTerm = 'iphone';
+    const searchParams = { searchTerm: mockSearchTerm };
+    const filteredProducts = products.filter(product =>
+      product.name.toLowerCase().includes(mockSearchTerm.toLowerCase())
+    );
+  
+    render(<Home searchParams={searchParams} />);
+  
+    const productCards = screen.getAllByTestId('product-card');
+    expect(productCards.length).toBe(filteredProducts.length); // Assert number of product cards matches filtered products
+  });
+
+  test('Displays all products when no search parameters are provided', () => {
+    const searchParams = {};
+    render(<Home searchParams={searchParams} />);
+  
+    const productCards = screen.getAllByTestId('product-card');
+    expect(productCards.length).toBe(products.length); // Assert number of product cards matches available products
+  });
--- app/components/products/ProductCard.tsx
+++ app/components/products/ProductCard.tsx
@@ -6,13 +6,14 @@ import { formatPrice } from '../../../utils/formatPrice'
 import { Rating } from '@mui/material'
 import { useRouter } from 'next/navigation'
 import { ProductData } from '../../../utils/products'
+import Link from 'next/link'
 
 interface ProductCardProps{
     data:ProductData
 }
 const ProductCard:React.FC<ProductCardProps> = ({data}) => {
     
-    const router = useRouter();
+
 
     const productRating = data.reviews.reduce((acc:number,item:any)=>
         item.rating + acc,0
@@ -20,7 +21,8 @@ const ProductCard:React.FC<ProductCardProps> = ({data}) => {
 
 
   return (
-    <div onClick={()=> router.push(`/product/${data.id}`)}  className='col-span-1 cursor-pointer border-[1.2px] border-slate-200 bg-slate-50 rounded-sm p-2 transition hover:scale-105 text-center text-sm'>
+    <Link href={`/product/${data.id}`}>
+    <div data-testid='product-card'  className='col-span-1 cursor-pointer border-[1.2px] border-slate-200 bg-slate-50 rounded-sm p-2 transition hover:scale-105 text-center text-sm' >
       <div className='flex flex-col items-center w-full gap-1 '>
         <div className='aspect-square overflow-hidden relative w-full'>
             <Image fill src={data.images[0].image} className='w-full h-full object-contain' alt={data.name}/>
@@ -39,6 +41,7 @@ const ProductCard:React.FC<ProductCardProps> = ({data}) => {
         </div>
       </div>
     </div>
+    </Link>
   )
 }
 
